5eb36ae3074d21506ce9b72473cbd4fd5583e9e4
Marc Hayes <marc.hayes.tech@gmail.com>
2020-08-15 23:52:38 -0600

    rebreak tests

--- puzzle_test.go
+++ puzzle_test.go
@@ -21,11 +21,10 @@ func testNewPuzzleFromStateFile(t *testing.T, stateFile string) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer p.IsSolved()
 
-	// if !p.IsSolved() {
-	// 	t.Fatal("Puzzle was not solved...")
-	// }
+	if !p.IsSolved() {
+		t.Fatal("Puzzle was not solved...")
+	}
 
 	afterSolveState, _ := p.State()
 	t.Logf("%#v", afterSolveState)
